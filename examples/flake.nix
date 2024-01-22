f99bc30d3fb17f4f54b001c37e56a590b56f0fc3
Simon Guest <simon.guest@agresearch.co.nz>
2024-01-23 11:40:22 +1300

    flake-utils has no nixpkgs input

--- examples/flake.nix
+++ examples/flake.nix
@@ -3,10 +3,7 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-unstable;
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
     kseq_split = {
       url = "github:AgResearch/kseq_split";
       inputs.nixpkgs.follows = "nixpkgs";
